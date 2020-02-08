f180c135d2df3c4c42f5a4dd95ca9fc52281b681
ayush <ayushksingh7@gmail.com>
2020-02-09 00:08:10 +0530

    Scanner

---
+++ app/src/main/java/com/example/scanner/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.scanner;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.zxing.Result;
+
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+public class MainActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler{
+    private ZXingScannerView zXingScannerView ;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void scan(View view){
+        zXingScannerView  =new ZXingScannerView(getApplicationContext());
+        setContentView(zXingScannerView);
+        zXingScannerView.setResultHandler(this);
+        zXingScannerView.startCamera();
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+    }
+
+    @Override
+    protected void onPause() {
+         super.onPause();
+         zXingScannerView.stopCamera();
+    }
+
+    @Override
+    public void handleResult(Result result) {
+
+        final String scanResult = result.getText();
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Scan Result");
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                zXingScannerView.resumeCameraPreview(MainActivity.this);
+            }
+        });
+        builder.setNeutralButton("VISIT", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Intent intent = new Intent(Intent.ACTION_VIEW , Uri.parse(scanResult));
+                startActivity(intent);
+            }
+        });
+        builder.setMessage(scanResult);
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+    }
+}
